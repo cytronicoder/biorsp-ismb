701c499a13fdc61f922a32d97c523ceca91b0066
Zeyu Yao <novodoodle@gmail.com>
2025-07-09 22:55:57 +0800

    KPMP study v1 (#1)

--- biorsp/__init__.py
+++ biorsp/__init__.py
@@ -1,5 +1,7 @@
-from . import utils, io, preprocess, spatial
+"""Top-level package for the BioRSP library."""
+
+from . import utils, io, preprocess, rsp
 from .preprocess import Preprocessor
-from .spatial import compute_rsp, compute_A1, compute_A2
+from .rsp import compute_rsp, compute_A1, compute_A2
 
 # spatial, plot, models, metrics
--- biorsp/rsp/__init__.py
+++ biorsp/rsp/__init__.py
@@ -1,3 +1,5 @@
-from .spatial import compute_rsp, compute_RMSD_with_permutation
+"""Convenience imports for the RSP (Radar Scanning Plot) API."""
+
+from .rsp import compute_rsp, compute_RMSD_with_permutation
 from .metrics import compute_A1, compute_A2
 from .helpers import compute_rsr, compute_histogram_area
